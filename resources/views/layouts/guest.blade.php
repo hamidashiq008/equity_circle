00b4de299096a0db489a44caed6b8a00b177e806
Mubashar Ahmad <mubashardev0203@gmail.com>
2024-11-11 19:36:12 +0500

    change title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>SocialV | Responsive Bootstrap 5 Admin Dashboard Template</title>
+        <title>Equity Circle</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
--- resources/views/layouts/withoutleftsidebar.blade.php
+++ resources/views/layouts/withoutleftsidebar.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>SocialV | Responsive Bootstrap 5 Admin Dashboard Template</title>
+    <title>Equity Circle</title>
 
     @include('partials._head')
 </head>
